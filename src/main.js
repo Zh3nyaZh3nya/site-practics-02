7f96ed357fd1c2543fb7b32b9600c9a8353f0074
Zh3nyaZh3nya <129783207+Zh3nyaZh3nya@users.noreply.github.com>
2023-05-19 19:54:37 +0600

    prepared the project for development

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import globalComponent from "@/components/UI/globalComponent";
 
-createApp(App).use(store).use(router).mount('#app')
+import "@/assets/scss/global.css";
+
+const app = createApp(App);
+
+globalComponent.forEach((component) => {
+    app.component(component.name, component);
+});
+
+app.use(store).use(router).mount('#app')
