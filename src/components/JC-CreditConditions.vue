bb0f034332c7329eef70f184f1e284301f29674e
Zh3nyaZh3nya <129783207+Zh3nyaZh3nya@users.noreply.github.com>
2023-05-25 00:25:31 +0600

    added a new component about credit conditions, added a new font

---
+++ src/components/JC-CreditConditions.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="conditions__wrapper">
+    <div class="conditions__content">
+      <div class="conditions__content-title component-title">
+        <h2>Основные условия <br/></h2>
+        <span>кредитования</span>
+      </div>
+      <div class="conditions__content__column">
+        <div class="conditions__content__column__left">
+          <div class="conditions__content__column__left-text">
+            <span>
+              Чтобы воспользоваться услугой и <br/>
+              купить авто в кредит, нужно <br/>
+              соответствовать простым <br/>
+              требованиям компании и объективно <br/>
+              оценивать свои возможности. <br/>
+              Как это работает? Выберите <br/>
+              объявление о продаже любой машины <br/>
+              и обращайтесь в Jet Car!
+            </span>
+          </div>
+        </div>
+        <div class="conditions__content__column__right">
+          <div
+              class="conditions__content__column__right-item"
+              v-for="item in conditionItem"
+              :key="item.id"
+          >
+            <div class="conditions__content__column__right-item-img">
+              <img :src="require('@/assets/image/imageCondition/svg/' + item.svg)" alt="">
+            </div>
+            <div class="conditions__content__column__right-item-text">
+              <span v-html="item.text"></span>
+            </div>
+            <div class="conditions__content__column__right-item-number">
+              <span>0{{item.id}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "jc-credit-conditions",
+  data() {
+    return {
+      conditionItem: [
+        {
+          id: 1,
+          svg: "year.svg",
+          text: "Вам от 18 до 65 лет",
+        },
+        {
+          id: 2,
+          svg: "home.svg",
+          text: "Вы проживаете в Алматы, <br /> Шымкенте, Таразе и их областях, <br /> а также являетесь гражданином <br /> Республики Казахстан",
+        },
+        {
+          id: 3,
+          svg: "identityCard.svg",
+          text: "У Вас есть действующее <br /> удостоверение личности <br /> и водительские права",
+        },
+        {
+          id: 4,
+          svg: "shield.svg",
+          text: "Приобретение страховки <br /> от несчастного случая 20% <br /> от суммы займа",
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/variables.scss";
+.conditions__wrapper {
+  max-width: 1140px;
+  width: 100%;
+  margin: 0 auto;
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 4px 4px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 50px;
+  .conditions__content {
+    padding: 61px 65px 84px 60px;
+    &-title {
+      margin-bottom: 30px;
+      h2 {
+        position: relative;
+        &:after {
+          content: "";
+          position: absolute;
+          bottom: 30px;
+          left: 421px;
+          background: url("@/assets/image/imageCondition/svg/J-title.svg") no-repeat;
+          display: block;
+          width: 59px;
+          height: 97px;
+        }
+      }
+    }
+    &__column {
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+      &__left {
+        &-text {
+          span {
+            font-size: $small-font-size;
+            line-height: $small-line-height;
+            color: #888888;
+          }
+        }
+      }
+      &__right {
+        display: flex;
+        flex-direction: column;
+        max-width: 400px;
+
+        row-gap: 22px;
+        &-item {
+          display: flex;
+          align-items: center;
+          column-gap: 29px;
+          &-img {
+            background: rgba(142, 191, 13, 0.1);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 16px;
+            border-radius: 50px;
+          }
+          &-text {
+            width: 338px;
+            span {
+              font: $small-font-size;
+              line-height: $small-line-height;
+            }
+          }
+          &-number {
+            span {
+              color: $green-color;
+              font-size: $medium-font-size;
+              line-height: $medium-line-height;
+              font-family: 'Josefin Sans', sans-serif;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/JC-MainPage.vue
+++ src/views/JC-MainPage.vue
@@ -4,6 +4,7 @@
   <jc-quiz />
   <jc-purchase-information />
   <jc-getting-loan />
+  <jc-credit-conditions />
 </template>
 
 <script>
@@ -11,9 +12,11 @@ import JcServices from "@/components/JC-Services";
 import JcQuiz from "@/components/JC-Quiz";
 import JcPurchaseInformation from "@/components/JC-PurchaseInformation";
 import JcGettingLoan from "@/components/JC-GettingLoan";
+import JcCreditConditions from "@/components/JC-CreditConditions";
 export default {
   name: "jc-main-page",
   components: {
+    JcCreditConditions,
     JcGettingLoan,
     JcPurchaseInformation,
     JcQuiz,
